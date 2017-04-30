92fd71f39aeb5947f05df845116ed6b4bde5f5dc
miaolegemitong <mitong1993@gmail.com>
2017-04-30 19:39:24 +0800

    add some

---
+++ src/main/java/com/miaolegemitong/leetcode/lcaofabst235/Solution.java
@@ -0,0 +1,24 @@
+package com.miaolegemitong.leetcode.lcaofabst235;
+
+import com.miaolegemitong.leetcode.common.TreeNode;
+
+/**
+ * @author mitong
+ * @email mitong@staff.sina.com.cn
+ * @date 2017/4/30
+ * @description
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ((root.val - p.val) * (root.val - q.val) <= 0) {
+            return root;
+        }
+        if (root.val - p.val > 0 && root.val - q.val > 0) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val - p.val < 0 && root.val - q.val < 0) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return null;
+    }
+}
